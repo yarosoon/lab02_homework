47a4d1efecb085dcac2d0bbd880c065ff5ded5c5
yarosoon <yaros.moon.ru@bk.ru>
2024-08-30 17:30:35 +0300

    fixed hello_world.cpp

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 int main() {
     string name;  
 
     
-    cout << "Введите имя пользователя: ";
-    getline(cin, name);  
+    std::cout << "Введите имя пользователя: ";
+    getline(std::cin, name);  
 
     
-    cout << "Hello world from " << name << "!" << endl;
+    std::cout << "Hello world from " << name << "!" << std::endl;
 
     return 0;
 }
