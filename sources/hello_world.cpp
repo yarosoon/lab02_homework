89efcb95884df5c61c4521223dc15b513f4ad058
yarosoon <yaros.moon.ru@bk.ru>
2024-08-30 17:36:31 +0300

    added comments to hello_world.cpp

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -2,13 +2,13 @@
 #include <string>
 
 int main() {
-    string name;  
+    string name;  // Создаем строку для хранения имени пользователя
 
-    
+    // Запрашиваем имя пользователя
     std::cout << "Введите имя пользователя: ";
-    getline(std::cin, name);  
+    std::getline(std::cin, name);  // Читаем строку целиком, включая пробелы
 
-    
+    // Печатаем пожелание
     std::cout << "Hello world from " << name << "!" << std::endl;
 
     return 0;
