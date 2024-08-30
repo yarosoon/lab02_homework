3eec0ef22d4195af1dfa9fb2e7afae1d9c030928
yarosoon <yaros.moon.ru@bk.ru>
2024-08-30 17:24:35 +0300

    fixed hello_world.cpp

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-> 
->using namespace std;
->
-> int main()
-> {
->   std::cout << "Hello, world!";
->   return 0;
-> }
-> EOF
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name;  
+
+    
+    cout << "Введите имя пользователя: ";
+    getline(cin, name);  
+
+    
+    cout << "Hello world from " << name << "!" << endl;
+
+    return 0;
+}
